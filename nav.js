18fdbe7e7e03130df938f64195f5dfaf18dc41db
kabirt7 <kabir.t7@hotmail.com>
2023-12-08 14:03:24 +1100

    outline of new carousel

--- nav.js
+++ nav.js
@@ -6,3 +6,21 @@ function toggleMenu() {
   const menuItems = document.querySelector(".nav__wrap");
   menuItems.classList.toggle("show");
 }
+
+function clickLeft() {
+  let container = document.querySelector(".work2__container");
+  container.scrollTo({
+    left: container.scrollLeft - 200,
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+function clickRight() {
+  let container = document.querySelector(".work2__container");
+  container.scrollTo({
+    left: container.scrollLeft + 200,
+    top: 0,
+    behavior: "smooth",
+  });
+}
